85f6053bbb885427d31aad16e22965545b266005
fgutierrezg <francisco.gutierrez.g@live.cl>
2025-01-29 13:13:03 -0300

    mejoras API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    findAll() {
+      return this.usersService.findAll();
+    }
+  
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+      return this.usersService.findOne(id);
+    }
+  
+    @Post()
+    create(@Body() createUserDto: { name: string }) {
+      return this.usersService.create(createUserDto);
+    }
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users = [{ id: 1, name: 'Juan' }];
+
+    findAll() {
+      return this.users;
+    }
+  
+    findOne(id: string) {
+      return this.users.find(user => user.id === Number(id));
+    }
+  
+    create(user: { name: string }) {
+      const newUser = { id: this.users.length + 1, ...user };
+      this.users.push(newUser);
+      return newUser;
+    }
+
+}
